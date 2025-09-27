a79c2fb8c94d85fb17ce294ebbcfee551583d594
catalogo <Martextiles@gmail.com>
2025-09-27 12:05:41 -0500

    Primer commit del catálogo tipo FlippingBook

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 // CÓDIGO CORREGIDO PARA ESPERAR A LAS IMÁGENES
 $(window).on('load', function() {
     
-    $('#catalog-book').turn({
-        width: 800,
-        height: 600,
-        autoCenter: true,
-        display: 'double',
-        acceleration: true
-    }).bind('turned', function(event, page) {
-        // Esta línea confirma que la librería está activa
-        console.log('Página volteada a: ' + page);
-    });
+    // Añade un pequeño retraso de 100 milisegundos
+    setTimeout(function() {
+        $('#catalog-book').turn({
+            width: 800,
+            height: 600,
+            autoCenter: true,
+            display: 'double',
+            acceleration: true
+        }).bind('turned', function(event, page) {
+             console.log('Página volteada a: ' + page);
+        });
+    }, 100); // 100ms de retraso
 });
